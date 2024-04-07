855ecc8f08ec18becaf3979ca64e6499f65a238c
Abhijit Roy <74091164+abhijitro@users.noreply.github.com>
2024-04-07 11:14:21 +0530

    Add files via upload

---
+++ EurekaServer/src/test/java/com/cts/spotify/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.spotify.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
