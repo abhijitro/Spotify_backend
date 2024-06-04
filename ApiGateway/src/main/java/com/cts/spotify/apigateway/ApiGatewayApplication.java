595c0f260c285cdb18c72d5f43260c77984446b1
Abhijit Roy <74091164+abhijitro@users.noreply.github.com>
2024-06-04 19:07:35 +0530

    Add files via upload

---
+++ ApiGateway/src/main/java/com/cts/spotify/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.cts.spotify.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@CrossOrigin(origins= "http://localhost:4200")
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
